a062d7468984a9ff3030edb94d7b1c3765f45c97
ne arkadiy <112948572+ArkadiGordin@users.noreply.github.com>
2024-01-26 10:06:09 +0200

    Add files via upload

---
+++ DatabaseTest/src/main/java/com/example/DatabaseTest/entity/Question.java
@@ -0,0 +1,32 @@
+package com.example.DatabaseTest.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String questionText;
+
+    public Question() {
+    }
+
+    public Question(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+}
